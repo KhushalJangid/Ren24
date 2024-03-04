d2aa053ff0493746d25b5cfaa703182b8a135ea1
KhushalJangid <kjangid897@gmail.com>
2024-03-04 19:55:32 +0530

    Sentry updated

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-from config import sentry
+import sentry_sdk
 from config import env
 
 # import razorpay
@@ -165,7 +165,16 @@ PRODUCTION = env.getParameter('PRODUCTION') == 'True'
 # PRODUCTION = False
 
 if PRODUCTION:
-    
+    sentry_sdk.init(
+        dsn="https://6ca2d413a490f99ba2658e6c92f6ed9e@o4506847830736896.ingest.sentry.io/4506847832506368",
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
     BASE_URL = env.getParameter('DOMAIN')
     
     # SECURITY WARNING: keep the secret key used in production secret!
