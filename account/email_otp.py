f6a142c210751013ad793af9755f5b043a0f5ba2
Amrinder Singh <amrindersingh292004@gmail.com>
2024-03-13 15:07:49 +0530

    Django smtp

--- account/email_otp.py
+++ account/email_otp.py
@@ -7,6 +7,7 @@ from botocore.exceptions import ClientError
 from config import settings
 from .models import User
 import requests
+from django.core.mail import send_mail
 
 # def send_otp(user:User,otp):
 #     SENDER = f"\"Your OTP for Renaissance Verification\" <{settings.Email}>"
@@ -126,54 +127,67 @@ import requests
 #     except requests.RequestException as e:
 #         print(f"Error sending OTP email: {e}")
 
-def send_otp(user:User, otp):
-    # Define your SMTP server settings
-    smtp_server = "smtp-relay.brevo.com"
-    smtp_port = 587
-    smtp_username = "ticket.renaissance@gmail.com"
-    smtp_password = "yfKJ7YwzOLRs6D9t"
+# def send_otp(user:User, otp):
+#     # Define your SMTP server settings
+#     smtp_server = "smtp-relay.brevo.com"
+#     smtp_port = 587
+#     smtp_username = "ticket.renaissance@gmail.com"
+#     smtp_password = "yfKJ7YwzOLRs6D9t"
 
-    # Define email parameters
-    sender = "OTP for Renaissance <ticket.renaissance@gmail.com"
-    recipient = user.email
-    name = f"{user.first_name} {user.last_name}"  # Replace with the user's name
-    subject = f"Your OTP: {otp}"
-    body_text = (f"Hi {name}, Your one-time password (OTP) for accessing your account is: {otp}. "
-                 f"This OTP is valid for 10 minutes. Please do not share it with anyone. "
-                 f"We hope you have a great time at Renaissance!")
-    body_html = f"""<html>
-    <head>Yor OTP for Renaissance Login</head>
-    <body>
-    <h1></h1>
-    <p>Hi {name},<br> 
-    Your one-time password (OTP) for accessing your account is:<br>
-    <b>{otp}</b><br> 
-    This OTP is valid for 10 minutes. Please do not share it with anyone. <br>
-    We hope you have a great time at Renaissance!<br>
-    Team JECRC Renaissance
-    </p>
-    </body>
-    </html>
-    """
+#     # Define email parameters
+#     sender = "OTP for Renaissance <ticket.renaissance@gmail.com"
+#     recipient = user.email
+#     name = f"{user.first_name} {user.last_name}"  # Replace with the user's name
+#     subject = f"Your OTP: {otp}"
+#     body_text = (f"Hi {name}, Your one-time password (OTP) for accessing your account is: {otp}. "
+#                  f"This OTP is valid for 10 minutes. Please do not share it with anyone. "
+#                  f"We hope you have a great time at Renaissance!")
+#     body_html = f"""<html>
+#     <head>Yor OTP for Renaissance Login</head>
+#     <body>
+#     <h1></h1>
+#     <p>Hi {name},<br> 
+#     Your one-time password (OTP) for accessing your account is:<br>
+#     <b>{otp}</b><br> 
+#     This OTP is valid for 10 minutes. Please do not share it with anyone. <br>
+#     We hope you have a great time at Renaissance!<br>
+#     Team JECRC Renaissance
+#     </p>
+#     </body>
+#     </html>
+#     """
 
-    # Create the email message
-    msg = MIMEMultipart("alternative")
-    msg["From"] = sender
-    msg["To"] = recipient
-    msg["Subject"] = subject
-    msg.attach(MIMEText(body_text, "plain"))
-    msg.attach(MIMEText(body_html, "html"))
+#     # Create the email message
+#     msg = MIMEMultipart("alternative")
+#     msg["From"] = sender
+#     msg["To"] = recipient
+#     msg["Subject"] = subject
+#     msg.attach(MIMEText(body_text, "plain"))
+#     msg.attach(MIMEText(body_html, "html"))
 
-    try:
-        # Connect to the SMTP server and send the email
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(smtp_username, smtp_password)
-            server.sendmail(sender, recipient, msg.as_string())  # Encode the message
-        print(f"OTP email sent successfully to {recipient}")
-    except smtplib.SMTPException as e:
-        print(f"Error sending OTP email: {e}")
+#     try:
+#         # Connect to the SMTP server and send the email
+#         with smtplib.SMTP(smtp_server, smtp_port) as server:
+#             server.starttls()
+#             server.login(smtp_username, smtp_password)
+#             server.sendmail(sender, recipient, msg.as_string())  # Encode the message
+#         print(f"OTP email sent successfully to {recipient}")
+#     except smtplib.SMTPException as e:
+#         print(f"Error sending OTP email: {e}")
         
+# def send_otp_thread(user:User, otp):
+#     t = Thread(target=send_otp, args=(user, otp))
+#     t.start()
+
+def send_email_otp(recipient,OTP):
+    name= f"{recipient.first_name} {recipient.last_name}"
+    otp=OTP
+    subject="OTP for your Registration"
+    message=f"Hi {name}, \n Your one-time password (OTP) for accessing your account is: {otp}. \n This OTP is valid for 10 minutes. Please do not share it with anyone. \n \n We hope you have a great time at Renaissance!"
+    from_email=settings.EMAIL_HOST_USER
+    recipient_list=[recipient]
+    send_mail(subject,message,from_email,recipient_list,fail_silently=True)
+    
 def send_otp_thread(user:User, otp):
-    t = Thread(target=send_otp, args=(user, otp))
+    t = Thread(target=send_email_otp, args=(user, otp))
     t.start()
